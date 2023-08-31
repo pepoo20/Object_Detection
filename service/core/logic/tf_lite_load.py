0d2495b0213de2907a1c337e414408bb6a620d1d
pepoo20 <85941953+pepoo20@users.noreply.github.com>
2023-09-01 00:14:09 +0700

    Update tf_lite_load.py

--- service/core/logic/tf_lite_load.py
+++ service/core/logic/tf_lite_load.py
@@ -2,7 +2,7 @@
 import numpy as np
 from tensorflow import lite as tf_lite
 def object_detection(img_array):
-    tflite_model_path = 'service\core\logic\pretrained_model.tflite'
+    tflite_model_path = 'service/core/logic/pretrained_model.tflite'
     interpreter = tf_lite.Interpreter(model_path=tflite_model_path)
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()
